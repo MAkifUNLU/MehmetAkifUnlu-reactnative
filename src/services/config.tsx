5f61b3654ef2fd6e901452ff9d16421264ed0b8c
Akif <akif.unlu44@gmail.com>
2022-11-21 17:29:22 +0300

    config file created and token added

---
+++ src/services/config.tsx
@@ -0,0 +1,7 @@
+const config = {
+    BASE_URL: 'https://upayments-studycase-api.herokuapp.com/api/',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFraWYudW5sdTQ0QGdhbWlsLmNvbSIsImdpdGh1YiI6Imh0dHBzOi8vZ2l0aHViLmNvbS9NQWtpZlVOTFUiLCJpYXQiOjE2Njg4ODA2NjQsImV4cCI6MTY2OTMxMjY2NH0.MEDSOJlrn7qioqCIWMtz1BLYeupD-perQ69YI041BFA',
+  };
+  
+  export default config;
