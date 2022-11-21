6d19c92ac1b39ccf9caf03ed9f8a743c6e841ae2
Akif <akif.unlu44@gmail.com>
2022-11-21 10:17:59 +0300

    navigation dependencies added

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/screens/HomeScreen';
-import { LandingScreen } from './src/screens/LandingScreeen';
+import { CreateScreen } from './src/screens/CreateScreen';
+import { LandingScreen } from './src/screens/LandingScreen';
 
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+function App() {
   return (
-    <HomeScreen/ >
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Create" component={CreateScreen} />
+      </Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
