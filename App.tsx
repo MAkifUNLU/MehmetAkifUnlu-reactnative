307ff9e6657fc6f9551a56147136a0dfdd12206a
Akif <akif.unlu44@gmail.com>
2022-11-24 15:29:25 +0300

    Detail screen added to navigation

--- App.tsx
+++ App.tsx
@@ -3,16 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/screens/HomeScreen';
 import { CreateScreen } from './src/screens/CreateScreen';
-import { LandingScreen } from './src/screens/LandingScreen';
+import { DetailScreen } from './src/screens/DetailScreen';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Create" component={CreateScreen} />
+          <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
 
     </NavigationContainer>
