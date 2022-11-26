e2383721de5a1a8f38e27e58780816bea55c9ba4
Akif <akif.unlu44@gmail.com>
2022-11-26 20:52:53 +0300

    token changed

--- src/services/config.tsx
+++ src/services/config.tsx
@@ -1,6 +1,6 @@
 const config = {
     BASE_URL: 'https://upayments-studycase-api.herokuapp.com/api/',
-    options: {headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFraWYudW5sdTQ0QGdhbWlsLmNvbSIsImdpdGh1YiI6Imh0dHBzOi8vZ2l0aHViLmNvbS9NQWtpZlVOTFUiLCJpYXQiOjE2Njg4ODA2NjQsImV4cCI6MTY2OTMxMjY2NH0.MEDSOJlrn7qioqCIWMtz1BLYeupD-perQ69YI041BFA' }},
+    options: {headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFraWYudW5sdTQ0QGdtYWlsLmNvbSIsImdpdGh1YiI6Imh0dHBzOi8vZ2l0aHViLmNvbS9NQWtpZlVOTFUiLCJpYXQiOjE2NjkzMjY1OTgsImV4cCI6MTY2OTc1ODU5OH0.7AibtGXYgjnL3pN93DWOWomD_eWzNxekGNbi_FpTlZ0' }},
   };
   
   export default config;
