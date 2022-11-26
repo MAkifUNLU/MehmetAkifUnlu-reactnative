2632d650bcd8b8de3dfbf24cc71a52994e06b5c1
Akif <akif.unlu44@gmail.com>
2022-11-26 20:53:31 +0300

    Middle ware added

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,18 +1,25 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, combineReducers, compose, configureStore } from '@reduxjs/toolkit';
 import ProductReducer from './reducers/ProductReducer';
+import { legacy_createStore as createStore} from 'redux'
 import CategoryReducer from './reducers/CategoryReducer';
+import thunk from 'redux-thunk';
 
 const reducers = combineReducers({
     product: ProductReducer,
     category: CategoryReducer,
 });
 
-export const store = configureStore({
-  reducer: reducers,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false,
-  }),
-});
+export const store = createStore(
+  reducers, applyMiddleware(thunk))
+
+// export const store = createStore(reducers, {}, compose(applyMiddleware(thunk)));
+
+// export const store = configureStore({
+//   reducer: reducers,
+//   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+//     serializableCheck: false,
+//   }),
+// });
 
 export default reducers;
 export type RootState = ReturnType<typeof reducers>;
